e0b902cba73831dc91927b0ae40d4ef7adc38a13
Anthony Zheng <bk01@uga.edu>
2014-11-12 15:58:13 -0500

    main

---
+++ src/main/java/cs1302/calc/Main.java
@@ -0,0 +1,40 @@
+package cs1302.calc;
+
+import javafx.fxml.FXMLLoader;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import java.io.IOException;
+ 
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    } // main
+    
+    @Override
+    public void start(Stage primaryStage) {
+	
+	try {
+	    Parent root = FXMLLoader.load(getClass().getResource("/calc.fxml"));
+	    primaryStage.setTitle("CalcFX!");
+	    primaryStage.setScene(new Scene(root, 330, 480));
+	    primaryStage.show();
+	} catch (Exception e) {
+	    System.out.println("There was a problem while running your program.");
+	    System.out.println("Please give the following information to the developer: ");
+	    System.out.println();
+	    System.out.println(e);
+	    System.out.println();
+	    e.printStackTrace();
+	    System.exit(1);
+	} // try
+
+    } // start
+
+} // Driver
